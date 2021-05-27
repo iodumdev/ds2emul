65e37021c436a62a4041f648d241479468b3699a
Denis <radiontcev@ivit.pro>
2021-05-27 10:51:39 +0300

    add commands

--- ds2emul.ino
+++ ds2emul.ino
@@ -6,7 +6,14 @@
 #define SS_PIN 10
 #define ACK_PIN 9
 
-uint8_t cmd[5] ={0x01, 0x42, 0x00, 0x00, 0x00};
+uint8_t pollCmd[5] ={0x01, 0x42, 0x00, 0x00, 0x00};
+uint8_t enterConfigCmd[5] ={0x01, 0x43, 0x00, 0x01, 0x00};
+uint8_t getInfoCmd1[9] ={0x01, 0x45, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t getInfoCmd2[9] ={0x01, 0x46, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+
+uint8_t *cmd;
+uint8_t cmd_index;
+uint8_t cmd_length;
 
 void setup(void)
 {
@@ -18,37 +25,50 @@ void setup(void)
   SPI.setDataMode(SPI_MODE3);
   SPI.setClockDivider(SPI_CLOCK_DIV64);
   digitalWrite(SS_PIN, HIGH);
-  
+
+  cmd = getInfoCmd1;
+
+  cmd_index = 0;
+  cmd_length = 4;
 }
 
 void loop(void)
 {
   digitalWrite(SS_PIN, LOW);
-  bool error = false;
 
-  for(uint8_t i=0; i<=4; i++)
+  switch(cmd_index){
+    case 0:
+      cmd = pollCmd;
+      cmd_length = 5;
+      cmd_index++;
+      break;
+      
+    case 1:
+      cmd = enterConfigCmd;
+      cmd_length = 5;
+      cmd_index++;
+      break;
+
+    case 2:
+      cmd = getInfoCmd1;
+      cmd_length = 9;
+      cmd_index++;
+      break;
+
+    case 3:
+      cmd = getInfoCmd2;
+      cmd_length = 9;
+      cmd_index = 0;
+      break;
+  }
+
+  for(uint8_t i=0; i <= cmd_length - 1; i++)
   {
-    //SPI.beginTransaction(SPISettings(1000000, LSBFIRST, SPI_MODE3));
+    delayMicroseconds(20);
     SPI.transfer(cmd[i]);
-    //SPI.endTransaction();
-   /*
-    uint8_t counter = 0;
-    if(i == 7) break;
-    while(!digitalRead(ACK_PIN))
-    {
-      counter++;
-      if(counter >= 100)
-      {
-        error = true;
-        break;
-      }
-      delay(10);
-    }
-    if(error) break;
-    */
-    delay(10);
+    delayMicroseconds(10);
   }
   
   digitalWrite(SS_PIN, HIGH);
-  delay(10);
+  delay(20);
 }
