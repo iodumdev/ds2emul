d3d70478f0a284b8bdbeb7c857298e4dfa2fafc4
Denis <radiontcev@ivit.pro>
2021-08-30 09:17:39 +0300

    update

--- ds2emul.ino
+++ ds2emul.ino
@@ -10,6 +10,15 @@ uint8_t pollCmd[5] ={0x01, 0x42, 0x00, 0x00, 0x00};
 uint8_t enterConfigCmd[5] ={0x01, 0x43, 0x00, 0x01, 0x00};
 uint8_t getInfoCmd1[9] ={0x01, 0x45, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
 uint8_t getInfoCmd2[9] ={0x01, 0x46, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t getInfoCmd3[9] ={0x01, 0x46, 0x00, 0x01, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t getInfoCmd4[9] ={0x01, 0x47, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t getInfoCmd5[9] ={0x01, 0x4C, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t getInfoCmd6[9] ={0x01, 0x4C, 0x00, 0x01, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t mapMotorCmd[9] ={0x01, 0x4D, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t findButtonsCmd[9] ={0x01, 0x41, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+uint8_t exitConfigCmd[9] ={0x01, 0x43, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+
+uint8_t *cmd_seq[] = {pollCmd, enterConfigCmd, getInfoCmd1, getInfoCmd2, getInfoCmd3, getInfoCmd4, getInfoCmd5, getInfoCmd6, mapMotorCmd, findButtonsCmd, exitConfigCmd};
 
 uint8_t *cmd;
 uint8_t cmd_index;
@@ -26,10 +35,10 @@ void setup(void)
   SPI.setClockDivider(SPI_CLOCK_DIV64);
   digitalWrite(SS_PIN, HIGH);
 
-  cmd = getInfoCmd1;
+  cmd = pollCmd;
 
   cmd_index = 0;
-  cmd_length = 4;
+  cmd_length = 5;
 }
 
 void loop(void)
@@ -37,28 +46,22 @@ void loop(void)
   digitalWrite(SS_PIN, LOW);
 
   switch(cmd_index){
-    case 0:
-      cmd = pollCmd;
+    
+   case 0:
+   case 1:
+      cmd = cmd_seq[cmd_index++];
       cmd_length = 5;
-      cmd_index++;
       break;
       
-    case 1:
-      cmd = enterConfigCmd;
-      cmd_length = 5;
-      cmd_index++;
-      break;
-
-    case 2:
-      cmd = getInfoCmd1;
-      cmd_length = 9;
-      cmd_index++;
-      break;
-
-    case 3:
-      cmd = getInfoCmd2;
+    case 10:
+      cmd = cmd_seq[cmd_index];
       cmd_length = 9;
       cmd_index = 0;
+      break;
+      
+   default:
+      cmd = cmd_seq[cmd_index++];
+      cmd_length = 9;
       break;
   }
 
